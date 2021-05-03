532b9647905671834391a8cb8e56ad50c1f381e5
Cacalo <cacalo219@hotmail.com>
2021-05-03 11:05:59 -0300

    QA13: Changed expect sentence

--- test/specs/automationTests.js
+++ test/specs/automationTests.js
@@ -11,8 +11,6 @@ describe("Wikipedia",() => {
 		let firstResult = $("#rso > div:nth-child(1) > div:nth-child(1) > div > div > div.yuRUbf > a > h3");
 		firstResult.click();
 		browser.pause(2000);
-		expect(browser.getUrl()).toMatch("https://es.wikipedia.org/wiki/Wikipedia:Portada");
+		expect(browser).toHaveUrl("https://es.wikipedia.org/wiki/Wikipedia:Portada");
 	})
-
-
 })
